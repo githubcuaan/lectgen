1e4329563a3c9f5731807242ca39c75c9ced8119
dnhfan <dinhan.0321@gmail.com>
2025-12-06 18:22:12 +0700

    feat(server): using cors allow client can req

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import { configDotenv } from "dotenv";
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 configDotenv();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Using middleware cors to connect Back - Front
+app.use(
+  cors({
+    origin: "http://localhost:5137",
+    methods: ["GET", "POST"],
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("fuck nguyen thanh binh");
