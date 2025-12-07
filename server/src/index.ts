e24f93ea7ffc559f71bbb95204397f23f64f40aa
dnhfan <dinhan.0321@gmail.com>
2025-12-07 21:44:13 +0700

    feat(back): conf port + use api route in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,25 @@
 import { configDotenv } from "dotenv";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import apiRoutes from "./routes/api.routes";
 
 configDotenv();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Using middleware cors to connect Back - Front
 app.use(
   cors({
-    origin: "http://localhost:5137",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
+    credentials: true,
   }),
 );
+
+app.use(express.json());
+
+app.use("/api", apiRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("fuck nguyen thanh binh");
